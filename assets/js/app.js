d7db3c6b7d4c1f4be69157a364933d861f33518f
youssef tadlaoui <usftadlaoui@gmail.com>
2022-10-21 16:08:09 +0100

    edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,81 +14,83 @@
    let toDo2 = document.getElementById('to-do-tasks2')
    let toDo3 = document.getElementById('to-do-tasks3')
    
+   
    function createTask() {
     // initialiser task form
     toDo.innerHTML=''
     toDo2.innerHTML=''
     toDo3.innerHTML=''
 
-    for(let i=0; i<data.length ;i++){
-        if( data[i].status=="To do"){
+    for(let i=0; i<tasks.length ;i++){
+        if( tasks[i].status=="To Do"){
              toDo.innerHTML+=`
-             <button class="w-100 border-0 d-flex bg-white border-bottom ">
+             <button onclick=editTask(${i}) class="w-100 border-0 d-flex bg-white border-bottom ">
 								<div class="col-1 mt-1">
 									<i class="bi bi-question-circle h4 text-green "></i> 
 								</div>
 								<div class="text-start  mt-1">
-									<div class="fw-bold">${data[i].title}</div>
+									<div class="fw-bold">${tasks[i].title}</div>
 									<div class="">
-										<div class="">#${i+1} created in ${data[i].date}</div>
-										<div class="" >${data[i].description} </div>
+										<div class="">#${i+1} created in ${tasks[i].date}</div>
+										<div class="" >${tasks[i].description} </div>
 									</div>
 									<div class="h5">
-										<span class="btn btn-sm btn-primary p-0 px-1 ">${data[i].priority}</span>
-										<span class="btn btn-sm btn-light text-black p-0 px-1 ">${data[i].type}</span>
+										<span class="btn btn-sm btn-primary p-0 px-1 ">${tasks[i].priority}</span>
+										<span class="btn btn-sm btn-light text-black p-0 px-1 ">${tasks[i].type}</span>
 									</div>
 				</div>
 			</button>
              `
-        }else if(data[i].status=="progress"){
+        }else if(tasks[i].status=="In Progress"){
             toDo2.innerHTML+= `
-            <button class="w-100 border-0 d-flex bg-white border-bottom ">
-                <div class="col-1 mt-1">
-                    <i class="spinner-border text-success spinner-border-sm "></i> 
+            <button onclick=editTask(${i}) class="w-100 border-0 d-flex bg-white border-bottom ">
+            <div class="col-1 mt-1">
+                <i class="spinner-border text-success spinner-border-sm"></i> 
+            </div>
+            <div class="text-start  mt-1">
+                <div class="fw-bold">${tasks[i].title}</div>
+                <div class="">
+                    <div class="">#${i+1} created in ${tasks[i].date}</div>
+                    <div class="" >${tasks[i].description} </div>
                 </div>
-                <div class="text-start col-11 mt-1">
-                    <div class="fw-bold">${data[i].title}</div>
-                    <div class="">
-                        <div class="">#${i+1} created in ${data[i].date}</div>
-                        <div class="">$</div>
-                    </div>
-                    <div class="h5">
-                        <span class="btn btn-sm btn-primary p-0 px-1 ">${data[i].priority}</span>
-                        <span class="btn btn-sm btn-light text-black p-0 px-1 ">${data[i].type}</span>
-                    </div>
+                <div class="h5">
+                    <span class="btn btn-sm btn-primary p-0 px-1 ">${tasks[i].priority}</span>
+                    <span class="btn btn-sm btn-light text-black p-0 px-1 ">${tasks[i].type}</span>
                 </div>
-            </button>
-        `}else if(data[i].status=="Done"){
+</div>
+</button>
+        `}else if(tasks[i].status=="Done"){
             toDo3.innerHTML+= `
-            <button class="w-100 border-0 d-flex bg-white border-bottom ">
-                <div class="col-1 mt-1">
-                    <i class="spinner-border text-success spinner-border-sm "></i> 
+            <button onclick=editTask(${i}) class="w-100 border-0 d-flex bg-white border-bottom ">
+            <div class="col-1 mt-1">
+                <i class="bi bi-check-circle h4 text-green "></i> 
+            </div>
+                <div class="text-start  mt-1">
+                <div class="fw-bold">${tasks[i].title}</div>
+                <div class="">
+                    <div class="">#${i+1} created in ${tasks[i].date}</div>
+                    <div class="" >${tasks[i].description} </div>
                 </div>
-                <div class="text-start col-11 mt-1">
-                    <div class="fw-bold">${data[i].title}</div>
-                    <div class="">
-                        <div class="">#${i+1} created in ${data[i].date}</div>
-                        <div class="">$</div>
+                     <div class="h5">
+                         <span class="btn btn-sm btn-primary p-0 px-1 ">${tasks[i].priority}</span>
+                         <span class="btn btn-sm btn-light text-black p-0 px-1 ">${tasks[i].type}</span>
                     </div>
-                    <div class="h5">
-                        <span class="btn btn-sm btn-primary p-0 px-1 ">${data[i].priority}</span>
-                        <span class="btn btn-sm btn-light text-black p-0 px-1 ">${data[i].type}</span>
-                    </div>
-                </div>
+               </div>
             </button>
-    `
+                             `
         }
     
-    // Afficher le boutton save
-    
-    // Ouvrir modal form
     
 }
    }
 
 function saveTask() {
+    if(saveTitle.value==''){
+      alert("remplis la form s'il vous plait")
+    }
     // Recuperer task attributes a partir les champs input
-    let objectsTask =
+    else{
+        let objectsTask =
     {
         title         : saveTitle.value,
         type          : document.querySelector(".form-check-input:checked").value,
@@ -97,19 +99,24 @@ function saveTask() {
         date          : recipientDate.value,
         description   : message.value,
     }
-    // Créez task object
-    data.push(objectsTask)
+    tasks.push(objectsTask)
     
     // Ajoutez object au Array
 
 
     // refresh tasks
-    console.log(data);
+    console.log(tasks);
     createTask()
+    reloadTasks()
+    }
+    // Créez task object
+   
     
     }
 
 function editTask(index) {
+    let taskIndex = index;
+    return taskIndex
     // Initialisez task form
 
     // Affichez updates
@@ -154,6 +161,13 @@ function initTaskForm() {
 
 function reloadTasks() {
     // Remove tasks elements
+    let inputs = document.querySelectorAll('input')
+    let tex = document.querySelector('textarea')
+inputs.forEach(input => {
+     input.value = ''
+    
+});
+tex.value = '';
    
      // Set Task coun
 }
--- assets/js/data.js
+++ assets/js/data.js
@@ -11,26 +11,6 @@ var tasks = [
         'description'   :   `There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.`,
     },
     {
-        'title'         :   'Consider creating an acceptance criteria list',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'To Do',
-        'date'          :   '2022-10-08',
-        'description'   :   `Descriptive requirements are very helpful when it comes to understanding the context of a problem, yet finally it is good to precisely specify what is expected. Thus the developer will not have to look for the actual requirements in a long, descriptive text but he will be able to easily get to the essence. One might find that sometimes — when acceptance criteria are well defined — there is little or no need for any additional information. Example:
-        a) User navigates to “/accounts” and clicks on red download CSV button
-        b) Popup appears with two buttons: “This year” and “Last year”
-        c) If user clicked on “Last year” download is initiated
-        d) CSV downloaded includes following columns…`,
-    },
-    {
-        'title'         :   'Provide mockups',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'To Do',
-        'date'          :   '2022-10-08',
-        'description'   :   `A textual requirements description is essential in most cases, but an image is often worth more than a thousand words. Even a simple mockup can limit misunderstandings by a great factor. There are many apps out there that might be helpful here, like Balsamiq, InVision or Mockingbird, but manipulating screenshots of an existing app also works.`,
-    },
-    {
         'title'         :   'Provide examples, credentials, etc',
         'type'          :   'Feature',
         'priority'      :   'High',
@@ -38,22 +18,7 @@ var tasks = [
         'date'          :   '2022-10-08',
         'description'   :   `If the expectation is to process or generate some file — attach an example of such a file. If the goal is to integrate what is being developed with some service, ensure your devs have access to this service and its documentation. This list could go on and on — the bottom line is — if there is something that our developer might make use of, try to foresee it and provide them with (access to) it.`,
     },
-    {
-        'title'         :   'Annotate',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'To Do',
-        'date'          :   '2022-10-08',
-        'description'   :   `The mockup provided can sometimes be confusing for developers. Especially if it contains much more content than the scope of the task described. Drop a couple of arrows, outlines and annotations here and there to emphasize what are the important parts of the mockup from the task requirements perspective.`,
-    },
-    {
-        'title'         :   'Use charts and diagrams',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'To Do',
-        'date'          :   '2022-10-08',
-        'description'   :   `While it is not always necessary, sometimes it might be beneficial to prepare a flowchart, a block diagram or some other kind of concept visualization that will render it easy for the developer to comprehend the task and its scope.`,
-    },
+   
     {
         'title'         :   'Spoil your developers with details',
         'type'          :   'Feature',
@@ -87,22 +52,7 @@ var tasks = [
         'date'          :   '2022-10-08',
         'description'   :   `including as many details as possible.`,
     },
-    {
-        'title'         :   'Provide access',
-        'type'          :   'Bug',
-        'priority'      :   'High',
-        'status'        :   'In Progress',
-        'date'          :   '2022-10-08',
-        'description'   :   `to the affected account and services if possible. It might be hard to reproduce the exact environment on a local machine.`,
-    },
-    {
-        'title'         :   'Provide environment information',
-        'type'          :   'Bug',
-        'priority'      :   'High',
-        'status'        :   'In Progress',
-        'date'          :   '2022-10-08',
-        'description'   :   `i.e., browser version, operating system version etc. Sometimes a list of installed browser plugins and extensions might be helpful as well.`,
-    },
+    
     {
         'title'         :   'Provide a link to an exception and/or a stack trace',
         'type'          :   'Bug',
@@ -127,20 +77,5 @@ var tasks = [
         'date'          :   '2022-10-08',
         'description'   :   `If it is possible and when it does not violate security policies, it is usually helpful for the developer to access the original data that might have played a role in the problem.`,
     },
-    {
-        'title'         :   'Make a screencast',
-        'type'          :   'Bug',
-        'priority'      :   'High',
-        'status'        :   'Done',
-        'date'          :   '2022-10-08',
-        'description'   :   `It is not always necessary, but many times a short screencast (or at least a screenshot) says more than a thousand words. While working on MacOS you can use QuickTime Player for the purpose but there are plenty of tools available for other operating systems as well.`,
-    },
-    {
-        'title'         :   'Provide contact information',
-        'type'          :   'Bug',
-        'priority'      :   'High',
-        'status'        :   'Done',
-        'date'          :   '2022-10-08',
-        'description'   :   `of the person that reported the bug. This will not always be possible, but in some cases it might be advantageous and most effective if a developer can have a chat with a person that actually experienced the bug, especially if the steps to reproduce a problem are not deterministic.`,
-    },
+    
 ];
